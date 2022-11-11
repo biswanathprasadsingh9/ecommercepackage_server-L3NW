ace61a70eeb7982f69be202964f88010a23cf12a
biswanathprasad <biswanath.prasad@conso4s.com>
2022-11-11 08:01:25 +0530

    '11NOV2022'

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -27,7 +27,7 @@ const getcartitems = (req,res) => {
                   model: 'Tax',
                 }]
              })
-              .populate('product_id',['name','sku','pricemain','stock','images','url'])
+              .populate('product_id',['name','sku','pricemain','stock','images','url','minimum_order','maximum_order'])
               // .sort({ _id: -1 })
               .then((response) => {
                 res.json({
@@ -39,6 +39,10 @@ const getcartitems = (req,res) => {
 
         resdata.forEach((item, i) => {
 
+
+          console.log(resdata.length)
+          console.log(i)
+
           //check record is already in user cart or not
           Cart.findOne({user_id:req.params.user_id,product_id:item.product_id},(err,doc)=>{
             if(doc===null){
@@ -47,29 +51,57 @@ const getcartitems = (req,res) => {
               Cart.findByIdAndUpdate(item._id,{$set:{ user_id:req.params.user_id}})
               .then(aasas=>{
 
-                    Cart.find({user_id:req.params.user_id})
-                      .sort({ _id: -1 })
-                      // .populate('user_id',['name','email'])
-                      // .populate('parent_product_id',['url','product_tax'])
-                      // .populate('product_id',['name','sku','pricemain','stock','images','url'])
-                      .populate('user_id',['name','email'])
-                      .populate({
-                        path: 'parent_product_id',
-                        select: 'url product_tax',
-                        populate: [
-                        {
-                          path: 'product_tax',
-                          model: 'Tax',
-                        }]
-                     })
-                      .populate('product_id',['name','sku','pricemain','stock','images','url'])
-                      // .sort({ _id: -1 })
-                      .then(response => {
-                        res.json({
-                          response: true,
-                          datas: response,
+
+                    if(resdata.length===i+1){
+                      // Cart.find({user_id:req.params.user_id})
+                      //   .sort({ _id: -1 })
+                      //   // .populate('user_id',['name','email'])
+                      //   // .populate('parent_product_id',['url','product_tax'])
+                      //   // .populate('product_id',['name','sku','pricemain','stock','images','url'])
+                      //   .populate('user_id',['name','email'])
+                      //   .populate({
+                      //     path: 'parent_product_id',
+                      //     select: 'url product_tax',
+                      //     populate: [
+                      //     {
+                      //       path: 'product_tax',
+                      //       model: 'Tax',
+                      //     }]
+                      //  })
+                      //   .populate('product_id',['name','sku','pricemain','stock','images','url','minimum_order','maximum_order'])
+                      //   // .sort({ _id: -1 })
+                      //   .then(responseq => {
+                      //     res.json({
+                      //       response: true,
+                      //       datas: responseq,
+                      //     });
+                      // });
+
+                      Cart.find({user_id:req.params.user_id})
+                        .sort({ _id: -1 })
+                        .populate('user_id',['name','email'])
+                        .populate({
+                          path: 'parent_product_id',
+                          select: 'url product_tax',
+                          populate: [
+                          {
+                            path: 'product_tax',
+                            model: 'Tax',
+                          }]
+                       })
+                        .populate('product_id',['name','sku','pricemain','stock','images','url','minimum_order','maximum_order'])
+                        // .sort({ _id: -1 })
+                        .then((response) => {
+                          res.json({
+                            response: true,
+                            datas: response,
+                          });
                         });
-                    });
+                    }
+
+
+
+
 
               })
 
@@ -89,25 +121,7 @@ const getcartitems = (req,res) => {
 
         });
 
-        // Cart.updateMany({system_id:req.params.system_id,user_id:null},{$set:{ user_id:req.params.user_id}})
-        // .then(ada=>{
-        //
-        //   // if(resdata.length===i+1){
-        //     Cart.find({user_id:req.params.user_id})
-        //       .sort({ _id: -1 })
-        //       .populate('user_id')
-        //       .populate('product_id',['name','sku','pricemain'])
-        //       // .sort({ _id: -1 })
-        //       .then(response => {
-        //         res.json({
-        //           response: true,
-        //           datas: response,
-        //         });
-        //     });
-        //
-        //   // }
-        //
-        // })
+
 
 
       }
@@ -130,7 +144,7 @@ const getcartitemsnologin = (req,res) => {
         model: 'Tax',
       }]
    })
-    .populate('product_id',['name','sku','pricemain','stock','images','url'])
+    .populate('product_id',['name','sku','pricemain','stock','images','url','minimum_order','maximum_order'])
     // .sort({ _id: -1 })
     .then((response) => {
       res.json({
@@ -150,6 +164,63 @@ const remove = (req,res) => {
       response:true
     })
   })
+}
+
+//<<<=== add item to cart from cart web page ===>>>
+
+const addtocart2 = (req,res) => {
+
+  Cart.findById(req.body.cart_item_id,(err,doc)=>{
+    if(doc===null){
+      res.json({
+        response:false,
+        message:'Failed'
+      })
+    }else{
+
+      Product.findById(doc.product_id,(err1,product)=>{
+
+        if(product===null){
+          res.json({
+            response:false,
+            message:'Product Removed'
+          })
+
+        }else{
+
+          var total_stock=product.stock;
+          var request_quantity=req.body.new_qty;
+
+          if(total_stock>=request_quantity){
+
+            const update = { quantity: request_quantity };
+            Cart.findByIdAndUpdate(req.body.cart_item_id,update)
+            .then(resupd=>{
+              res.json({
+                response:true,
+                body:req.body,
+                product,
+                message:'Success'
+              })
+            })
+
+          }else{
+
+            res.json({
+              response:false,
+              message:`Sorry only ${total_stock} available`
+            })
+
+          }
+
+        }
+
+      })
+
+    }
+
+  })
+
 }
 
 
@@ -275,5 +346,5 @@ const store = (req,res) => {
 
 
 module.exports = {
-  store,addcheckcart,gencartitemlocal,getcartitems,getcartitemsnologin,remove
+  store,addcheckcart,gencartitemlocal,getcartitems,getcartitemsnologin,remove,addtocart2
 };
--- routes/cart.js
+++ routes/cart.js
@@ -8,6 +8,8 @@ router.get('/getcartitems/:user_id/:system_id',CartController.getcartitems);
 router.get('/getcartitemsnologin/:system_id',CartController.getcartitemsnologin);
 router.get('/remove/:id',CartController.remove);
 router.post('/addcheckcart',CartController.addcheckcart);
+router.post('/addtocart2',CartController.addtocart2);
+
 router.post('/gen_cart_item_local',CartController.gencartitemlocal);
 
 module.exports=router;
