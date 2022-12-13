4f5cd81efb09b47397e8587aa7a0ed4a60ec8677
biswanathprasad <biswanath.prasad@conso4s.com>
2022-12-13 08:32:43 +0530

    '13DEC2022'

--- server.js
+++ server.js
@@ -540,7 +540,7 @@ const client = new Client();
 
 
   client.on('qr', qr => {
-      qrcode.generate(qr, {small: true});
+      // qrcode.generate(qr, {small: true});
   });
 
   client.on('ready', () => {
